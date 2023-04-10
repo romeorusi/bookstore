d5c517cc02f1a727b5c43c3def5ceb4af7113d99
romeorusi <Romeo.rusi@gmail.com>
2023-04-10 17:08:43 +0300

    add book functionality

--- src/main/java/rr/bookstore/web/BookController.java
+++ src/main/java/rr/bookstore/web/BookController.java
@@ -7,7 +7,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import rr.bookstore.domain.Book;
 import rr.bookstore.domain.BookRepository;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 
 @Controller
@@ -19,7 +23,19 @@ private BookRepository bookRepository;
 @RequestMapping(value={"/booklist"})
 public String bookList(Model model) {
     model.addAttribute ("books", bookRepository.findAll());
-    return "booklist";
-    
+    return "booklist";    
 }
+@RequestMapping(value="/add")
+public String addBook(Model model) {
+    model.addAttribute("book", new Book());
+    return "addbook";
+}
+@PostMapping(value="/save")
+public String save(Book book) {
+    //TODO: process POST request
+    bookRepository.save(book);
+    
+    return "redirect:booklist";
+}
+
 }
