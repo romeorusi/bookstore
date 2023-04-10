00dd576d6d545e74c9b4c5f27634802e0a0c9e03
romeorusi <Romeo.rusi@gmail.com>
2023-04-10 18:02:40 +0300

    edit and css functionality

--- src/main/java/rr/bookstore/web/BookController.java
+++ src/main/java/rr/bookstore/web/BookController.java
@@ -22,16 +22,19 @@ public class BookController {
 @Autowired
 private BookRepository bookRepository;
 
+//login
 @RequestMapping(value={"/booklist"})
 public String bookList(Model model) {
     model.addAttribute ("books", bookRepository.findAll());
     return "booklist";    
 }
+//add
 @RequestMapping(value="/add")
 public String addBook(Model model) {
     model.addAttribute("book", new Book());
     return "addbook";
 }
+//save
 @PostMapping(value="/save")
 public String save(Book book) {
     //TODO: process POST request
@@ -39,10 +42,18 @@ public String save(Book book) {
     
     return "redirect:booklist";
 }
-
+//delete
 @RequestMapping(value="/delete/{id}", method=RequestMethod.GET)
 public String deleteBook(@PathVariable("id") Long bookId, Model model) {
     bookRepository.deleteById(bookId);
     return "redirect:../booklist";
+}
+//editing
+@RequestMapping(value="/edit/{id}", method=RequestMethod.GET)
+public String editBook(@PathVariable("id") Long bookId, Model model){
+
+    model.addAttribute("book", bookRepository.findById(bookId));
+    return "editbook";
+
 }
 }
